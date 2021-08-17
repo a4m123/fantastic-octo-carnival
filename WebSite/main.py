b5c754fda36b7a70497228cbe07969e2e4404ff0
Артем Малкин <artem.malkin.2000@gmail.com>
2021-08-17 10:20:50 +0300

    test

--- WebSite/main.py
+++ WebSite/main.py
@@ -1,25 +1,59 @@
+from PyQt5.QtCore import QThread, pyqtSignal
 import cv2 as cv
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QApplication, QGridLayout, QMainWindow, QWidget, QLabel
+from PyQt5.QtCore import QThread, Qt, pyqtSlot
 
-#print("Before URL")
-#cap = (cv.VideoCapture('http://admin:admin@192.168.10.11:80'))
-cap = (cv.VideoCapture('rtsp://admin:admin@192.168.10.12:554/live/main'))
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+class Thread(QThread):
+   change_pixmap = pyqtSignal(QImage)
+   def run(self):
+    cap = (cv.VideoCapture('rtsp://admin:admin@192.168.10.12:554/live/main'))
+    while(True):
+            ret, frame = cap.read()
+            if ret:
+                rgb_image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+                h, w, ch = rgb_image.shape
+                bytes_per_line = ch * w
+                convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+                p = convert_to_Qt_format.scaled(640, 480, Qt.KeepAspectRatio)
+                self.change_pixmap.emit(p)
+                
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    
+    @pyqtSlot(QImage)
+    def setImage(self, image):
+        self.label.setPixmap(QPixmap.fromImage(image))
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.resize(1800, 1200)
+        # create a label
+        self.label = QLabel(self)
+        self.label.move(280, 120)
+        self.label.resize(640, 480)
+        th = Thread(self)
+        th.changePixmap.connect(self.setImage)
+        th.start()
+        self.show()
+        '''
+        self.grid_layout = QGridLayout()
+        self.screen_resolution = QApplication.desktop().availableGeometry()
+        self.show_stream = QImage()
+        '''
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    # stylesheetfile = "dark-orange-green.qss"
+    # with open(stylesheetfile,"r") as fh:
+    #     app.setStyleSheet(fh.read())
+    main = mainForm()
+    main.showMaximized()
+    ret = app.exec_()
+    if main.edge_finder is not None:
+        if main.edge_finder.isRunning():
+            main.edge_finder.terminate()
+    sys.exit(ret)
